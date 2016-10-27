1dd7c8255d7c6d8d02e6f37e5c16379e1b24a6ac
soulefix <souleyfix@yopmail.com>
2016-10-27 09:31:41 +0200

    Suppression d'une ligne vide

--- 02_ProjetGithubMaven/src/main/java/com/m2i/gitMaven/Main.java
+++ 02_ProjetGithubMaven/src/main/java/com/m2i/gitMaven/Main.java
@@ -3,10 +3,8 @@ package com.m2i.gitMaven;
 public class Main {
 
 	public static void main(String[] args) {
-
 		Message obj = new Message();
 		System.out.println(obj.getMessage("Jenkins"));
-
 	}
 
 }
